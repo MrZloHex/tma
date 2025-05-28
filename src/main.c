e09997f7f1cfd72d4bba953918f98bb52b54a605
MrZloHex <zlo.alex.it@gmail.com>
2025-05-28 20:06:02 +0300

    log to file

--- src/main.c
+++ src/main.c
@@ -20,9 +20,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include <getopt.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 #include "tma.h"
 #include "util.h"
@@ -34,6 +37,7 @@ usage(const char *program_name)
     printf("Usage: %s [OPTIONS]\n", program_name);
     printf("\nOptions:\n");
     printf("  -p, --port <port>       Specify the port number to host on (1-65535).\n");
+    printf("  -F, --file              Filename to write logs.\n");
     printf("  -f, --fork              Fork process.\n");
     printf("  -v, --verbose           More verbose logs.\n");
     printf("  -h, --help              Display this help message and exit.\n");
@@ -47,10 +51,12 @@ main(int argc, char *argv[])
 {
     int   port       = 0;
     bool  verbose    = false;
+    char *file       = NULL;
 
     static struct option long_options[] =
     {
         {"port",        required_argument,  0, 'p'},
+        {"file",        required_argument,  0, 'F'},
         {"verbose",     no_argument,        0, 'v'},
         {"fork",        no_argument,        0, 'f'},
         {"help",        no_argument,        0, 'h'},
@@ -60,7 +66,7 @@ main(int argc, char *argv[])
     int opt;
     int option_index = 0;
 
-    while ((opt = getopt_long(argc, argv, "p:vfh", long_options, &option_index)) != -1)
+    while ((opt = getopt_long(argc, argv, "p:F:vfh", long_options, &option_index)) != -1)
     {
         switch (opt)
         {
@@ -91,6 +97,13 @@ main(int argc, char *argv[])
             usage(argv[0]);
             return 0;
 
+        case 'F':
+            file = (char *)calloc(strlen(optarg), sizeof(char));
+            if (!file)
+            { exit(1); }
+            strcpy(file, optarg);
+            break;
+
         default:
             fprintf(stderr, "Error: Unknown argument.\n");
             usage(argv[0]);
@@ -103,6 +116,30 @@ main(int argc, char *argv[])
         fprintf(stderr, "Error: Missing required arguments.\n");
         usage(argv[0]);
         return 1;
+    }
+    if (file)
+    {
+        int fd = open(file, O_WRONLY|O_CREAT|O_APPEND,  S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+
+        if (fd < 0)
+        {
+            perror("open log");
+            return EXIT_FAILURE;
+        }
+
+        if
+        (
+            dup2(fd, STDOUT_FILENO) < 0 ||
+            dup2(fd, STDERR_FILENO) < 0
+        )
+        {
+            perror("dup2");
+        }
+
+        close(fd);
+
+        setvbuf(stdout, NULL, _IONBF, 0);
+        setvbuf(stderr, NULL, _IONBF, 0);
     }
 
     tma(port, verbose);
